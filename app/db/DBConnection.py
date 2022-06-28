b36aafc6999feba4db8df1b2c5c3c02c93df9f0c
Dev2 - Jacopo <jacopo_ba@outlook.it>
2022-06-28 18:01:47 +0200

    backend in master

---
+++ app/db/DBConnection.py
@@ -0,0 +1,36 @@
+import mysql.connector
+import json
+
+class DBUtility:
+    @staticmethod
+    def __connectToDB__():
+        # specifiche di backup del DB in caso il try fallisca
+        data = {
+            "endpoint" : "localhost",
+            "user" : "root",
+            "password" : "UltrasLiberi1908.",
+            "database" : "task2406"
+        }
+        try:  
+            with open('DBConfig.json') as f:
+                data = json.load(f)
+                return data
+        except:
+            return data
+    
+    @staticmethod
+    def getConnection(data = __connectToDB__()):
+        connessione = None
+        try:
+            # Connessione a MySQL
+            connessione = mysql.connector.connect(
+            # Parametri per la connessione
+            host=data["endpoint"],
+            user=data["user"],
+            password=data["password"],
+            database=data["database"])
+            print(connessione)
+        except mysql.connector.Error as e:
+            print("Error reading data from MySQL table", e)
+        finally:
+            return connessione
---
+++ app/db/RisultatiTest.py
@@ -0,0 +1,53 @@
+import json
+import mysql.connector
+from datetime import datetime
+from .DBConnection import DBUtility
+# creo la classe RisultatiTes che è quella che andrà a gestire la stampa a schermo e l'inserimento nel db dei risultati del quiz
+class RisultatiTest:
+    @staticmethod
+    def getClassificaFinale():
+        connessione = DBUtility.getConnection()
+        classificaFinale = []
+        try:
+            # Generazione del cursore
+            cursore = connessione.cursor()
+            # Comando SQL per la visualizzazione dei database in formato SQL
+            cursore.execute("select U.nome, U.cognome, TR.score, TR.data_test from utente U, test_risultati TR where TR.fk_utente = U.id_utente ")
+            # get all records
+            records = cursore.fetchall()
+            # salva tutti gli elementi del record in una lista 
+            for elem in records:
+                classificaFinale.append(elem)
+        except mysql.connector.Error as e:
+                    print("Error reading data from MySQL table", e)
+        finally:
+            if connessione.is_connected():
+                connessione.close()
+                cursore.close()
+        
+        return json.dumps(classificaFinale)
+
+    @staticmethod 
+    def addValuesInClassifica(id_utente, score, data):
+        connessione = DBUtility.getConnection()
+        somma = 0
+        try:
+            # Generazione del cursore
+            cursore = connessione.cursor()
+            # Query SQL per ottenere lo score da sommare
+            query1 = """SELECT score FROM test_risultati WHERE fk_utente = %s;"""
+            cursore.execute(query1, (id_utente, ))
+            # prendo il record dal cursore con il fetchall() e accedo all'int mediante un doppio indice, in quanto all'interno della lista dei records ho una tupla contenente il valore del campo score
+            records = cursore.fetchall()
+            somma = records[0][0] + score
+            # Query SQL per l'inserimento dei valori nel database in formato SQL
+            query2 = """UPDATE test_risultati SET score = %s, data_test = %s WHERE fk_utente = %s;COMMIT;"""
+            cursore.execute(query2, (somma, data, id_utente))
+            return print(f"""Modifica avvenuta con successo, aggiunti {score} punti, in data {data}, all'utente {id_utente}, ora ha {somma} punti""")
+        except mysql.connector.Error as e:
+                    print("Error reading data from MySQL table", e)
+        finally:
+            if connessione.is_connected():
+                connessione.close()
+                cursore.close()
+    
