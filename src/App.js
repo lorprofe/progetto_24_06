bde64fb7b7150beea93446b4b028f300962a4660
Valerio - Dangiolella <nello.dangiolella.vda@gmail.com>
2022-06-28 15:16:16 +0200

    caricamento-dati

--- src/App.js
+++ src/App.js
@@ -1,67 +1,81 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
+import './script';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="start_btn"><button>Start Quiz</button></div>
+    <div className="info_box">
+      <div className="info-title"><span>Rules Game</span></div>
+      <div className="info-list">
+        <div className="info">1. Avrai 15 secondi di tempo ad ogni risposta!<span /> </div>
+        <div className="info">2. Usa il tuo sapere non consultare Internet!</div>
+        <div className="info">3. Alla fine del Quiz potrai consultare il risulato!!</div>
+        <div className="info">5. Se fai piu di 5 errori è meglio che approfondisci la tua coltura :(</div>
+      </div>
+      <div className="buttons">
+        <button className="quit">Exit Quiz</button>
+        <button className="restart">Continue</button>
+      </div>
     </div>
-  );
+    <div className="quiz_box">
+      <header>
+        <div className="title">Awesome Quiz Application</div>
+        <div className="timer">
+          <div className="time_left_txt">Time Left</div>
+          <div className="timer_sec">15</div>
+        </div>
+        <div className="time_line" />
+      </header>
+      <section>
+        <div className="que_text">
+        </div>
+        <div className="option_list">
+        </div>
+      </section>
+      <footer>
+        <div className="total_que">
+        </div>
+        <button id="next-question" className="next_btn">Next Question</button>
+      </footer>
+    </div>
+    <div className="result_box">
+      <div className="icon">
+        <i className="fas fa-crown" />
+      </div>
+      <div className="complete_text">You've completed the Quiz!</div>
+      <div className="score_text">
+        -&gt;
+      </div>
+      <div className="buttons">
+        <button className="restart">Replay Quiz</button>
+        <button className="quit">Quit Quiz</button>
+      </div>
+    </div>
+    <footer>
+      <div id="form">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <br />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Password:</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div><button id="btn" type="submit" className="btn btn-primary">Submit</button></div>
+        </form>
+      </div>
+    </footer>
+  </div>
+);
 }
+
+
 
 export default App;
 
 
-<div>
-  <div class="accordion" id="accordionPanelsStayOpenExample">
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-          Accordion Item #1
-        </button>
-      </h2>
-      <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-        <div class="accordion-body">
-          <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-          Accordion Item #2
-        </button>
-      </h2>
-      <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-        <div class="accordion-body">
-          <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-          Accordion Item #3
-        </button>
-      </h2>
-      <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-        <div class="accordion-body">
-          <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
