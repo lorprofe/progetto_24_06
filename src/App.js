8f196baca54aadd5b1ca1853be5d786af3844290
Valerio - Dangiolella <nello.dangiolella.vda@gmail.com>
2022-06-28 16:29:08 +0200

    caricamento-dati

--- src/App.js
+++ src/App.js
@@ -1,81 +1,174 @@
-import React, {useState, useEffect} from 'react';
+
 
 import './App.css';
 import './script';
 
-function App() {
-  return (
-    <div>
-    <div className="start_btn"><button>Start Quiz</button></div>
-    <div className="info_box">
-      <div className="info-title"><span>Rules Game</span></div>
-      <div className="info-list">
-        <div className="info">1. Avrai 15 secondi di tempo ad ogni risposta!<span /> </div>
-        <div className="info">2. Usa il tuo sapere non consultare Internet!</div>
-        <div className="info">3. Alla fine del Quiz potrai consultare il risulato!!</div>
-        <div className="info">5. Se fai piu di 5 errori è meglio che approfondisci la tua coltura :(</div>
-      </div>
-      <div className="buttons">
-        <button className="quit">Exit Quiz</button>
-        <button className="restart">Continue</button>
-      </div>
-    </div>
-    <div className="quiz_box">
-      <header>
-        <div className="title">Awesome Quiz Application</div>
-        <div className="timer">
-          <div className="time_left_txt">Time Left</div>
-          <div className="timer_sec">15</div>
+import React, { useState } from 'react';
+
+export default function App() {
+	const questions = [
+		{
+			questionText: 'Quale città ha più abitanti?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'Pechino', isCorrect: false },
+				{ answerText: 'New Dheli', isCorrect: true },
+				{ answerText: 'Tokyo', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Quali sono i colori sociali di Roma?',
+			answerOptions: [
+				{ answerText: 'Bianco e Celeste', isCorrect: false },
+				{ answerText: 'Giallo e Rosso', isCorrect: true },
+				{ answerText: 'Giallo e Blu', isCorrect: false },
+				{ answerText: 'Bianco e Giallo', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Quale animale gloglotta?',
+			answerOptions: [
+				{ answerText: 'Tacchino', isCorrect: true },
+				{ answerText: 'Passerotto', isCorrect: false },
+				{ answerText: 'Cane', isCorrect: false },
+				{ answerText: 'Maiale', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'In che anno è stata scoperta lAmerica',
+			answerOptions: [
+				{ answerText: '1512', isCorrect: false },
+				{ answerText: '1500', isCorrect: false },
+				{ answerText: '1489', isCorrect: false },
+				{ answerText: '1492', isCorrect: true },
+			],
+		},
+    {
+			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+			answerOptions: [
+				{ answerText: 'Italia', isCorrect: false },
+				{ answerText: 'Brasile', isCorrect: true },
+				{ answerText: 'Francia', isCorrect: false },
+				{ answerText: 'Inghilterra', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'La Bandiera di quale nazione non ha una forma rettangolare',
+			answerOptions: [
+				{ answerText: 'St.Kitts & Nevis', isCorrect: false },
+				{ answerText: 'Nepal', isCorrect: true },
+				{ answerText: 'Isole Vergini Britanniche ', isCorrect: false },
+				{ answerText: 'Taipei', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'Quando è finita la Seconda Guerra Mondiale',
+			answerOptions: [
+				{ answerText: '1943', isCorrect: false },
+				{ answerText: '1945', isCorrect: true },
+				{ answerText: '1944', isCorrect: false },
+				{ answerText: '1946', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'Quali città furono colpite dalla bomba atomica?',
+			answerOptions: [
+				{ answerText: ' Sapporo e Sendai', isCorrect: false },
+				{ answerText: ' Hiroshima e Nagasaki', isCorrect: true },
+				{ answerText: ' Kyoto e Nagoya', isCorrect: false },
+				{ answerText: 'Kumamoto e Sagamihara ', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+			answerOptions: [
+				{ answerText: ' ', isCorrect: false },
+				{ answerText: ' ', isCorrect: true },
+				{ answerText: ' ', isCorrect: false },
+				{ answerText: ' ', isCorrect: false },
+			],
+		},
+    {
+			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+			answerOptions: [
+				{ answerText: ' ', isCorrect: false },
+				{ answerText: ' ', isCorrect: true },
+				{ answerText: ' ', isCorrect: false },
+				{ answerText: ' ', isCorrect: false },
+			],
+		},
+	];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	
+	  return (
+  		
+  		  <div>
+  		    <div className='app'>
+      			{showScore ? (
+      				<div className='score-section'>
+      					You scored {score} out of {questions.length}
+      				</div>
+      			) : (
+      				<>
+      					<div className='question-section'>
+      						<div className='question-count'>
+      							<span>Question {currentQuestion + 1}</span>/{questions.length}
+      						</div>
+      						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+      					</div>
+      					<div className='answer-section'>
+      						{questions[currentQuestion].answerOptions.map((answerOption) => (
+      							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+      						))}
+      					</div>
+      				</>
+      			)}
+            
+  		  </div>
+        
+       <footer>
+        <div id="form">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              <br />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">Password:</label>
+              <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <div><button id="btn" type="submit" className="btn btn-primary">Submit</button></div>
+          </form>
         </div>
-        <div className="time_line" />
-      </header>
-      <section>
-        <div className="que_text">
-        </div>
-        <div className="option_list">
-        </div>
-      </section>
-      <footer>
-        <div className="total_que">
-        </div>
-        <button id="next-question" className="next_btn">Next Question</button>
       </footer>
-    </div>
-    <div className="result_box">
-      <div className="icon">
-        <i className="fas fa-crown" />
-      </div>
-      <div className="complete_text">You've completed the Quiz!</div>
-      <div className="score_text">
-        -&gt;
-      </div>
-      <div className="buttons">
-        <button className="restart">Replay Quiz</button>
-        <button className="quit">Quit Quiz</button>
-      </div>
-    </div>
-    <footer>
-      <div id="form">
-        <form>
-          <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">Email:</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <br />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleInputPassword1" className="form-label">Password:</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
-          </div>
-          <div><button id="btn" type="submit" className="btn btn-primary">Submit</button></div>
-        </form>
-      </div>
-    </footer>
-  </div>
-);
-}
+
+    		</div>
+
+          );
+        }
+        
+ 
 
 
 
-export default App;
+
+
 
 
