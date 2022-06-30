b022075a43b43013cb23a3cd6b1ba8eaa63a2865
Dangio02 <nello.dangiolella.vda@gmail.com>
2022-06-30 11:18:58 +0200

    caricamento-dati

--- src/App.js
+++ src/App.js
@@ -8,98 +8,100 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
 
 
-	const questions = [
-		{
-			questionText: 'Quale città ha più abitanti?',
-			answerOptions: [
-				{ answerText: 'New York', isCorrect: false },
-				{ answerText: 'Pechino', isCorrect: false },
-				{ answerText: 'New Dheli', isCorrect: true },
-				{ answerText: 'Tokyo', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Quali sono i colori sociali di Roma?',
-			answerOptions: [
-				{ answerText: 'Bianco e Celeste', isCorrect: false },
-				{ answerText: 'Giallo e Rosso', isCorrect: true },
-				{ answerText: 'Giallo e Blu', isCorrect: false },
-				{ answerText: 'Bianco e Giallo', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'Quale animale gloglotta?',
-			answerOptions: [
-				{ answerText: 'Tacchino', isCorrect: true },
-				{ answerText: 'Passerotto', isCorrect: false },
-				{ answerText: 'Cane', isCorrect: false },
-				{ answerText: 'Maiale', isCorrect: false },
-			],
-		},
-		{
-			questionText: 'In che anno è stata scoperta lAmerica',
-			answerOptions: [
-				{ answerText: '1512', isCorrect: false },
-				{ answerText: '1500', isCorrect: false },
-				{ answerText: '1489', isCorrect: false },
-				{ answerText: '1492', isCorrect: true },
-			],
-		},
-    {
-			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
-			answerOptions: [
-				{ answerText: 'Italia', isCorrect: false },
-				{ answerText: 'Brasile', isCorrect: true },
-				{ answerText: 'Francia', isCorrect: false },
-				{ answerText: 'Inghilterra', isCorrect: false },
-			],
-		},
-    {
-			questionText: 'La Bandiera di quale nazione non ha una forma rettangolare',
-			answerOptions: [
-				{ answerText: 'St.Kitts & Nevis', isCorrect: false },
-				{ answerText: 'Nepal', isCorrect: true },
-				{ answerText: 'Isole Vergini Britanniche ', isCorrect: false },
-				{ answerText: 'Taipei', isCorrect: false },
-			],
-		},
-    {
-			questionText: 'Quando è finita la Seconda Guerra Mondiale',
-			answerOptions: [
-				{ answerText: '1943', isCorrect: false },
-				{ answerText: '1945', isCorrect: true },
-				{ answerText: '1944', isCorrect: false },
-				{ answerText: '1946', isCorrect: false },
-			],
-		},
-    {
-			questionText: 'Quali città furono colpite dalla bomba atomica?',
-			answerOptions: [
-				{ answerText: ' Sapporo e Sendai', isCorrect: false },
-				{ answerText: ' Hiroshima e Nagasaki', isCorrect: true },
-				{ answerText: ' Kyoto e Nagoya', isCorrect: false },
-				{ answerText: 'Kumamoto e Sagamihara ', isCorrect: false },
-			],
-		},
-    {
-			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
-			answerOptions: [
-				{ answerText: ' ', isCorrect: false },
-				{ answerText: ' ', isCorrect: true },
-				{ answerText: ' ', isCorrect: false },
-				{ answerText: ' ', isCorrect: false },
-			],
-		},
-    {
-			questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
-			answerOptions: [
-				{ answerText: ' ', isCorrect: false },
-				{ answerText: ' ', isCorrect: true },
-				{ answerText: ' ', isCorrect: false },
-				{ answerText: ' ', isCorrect: false },
-			],
-		},
-	];
+      /*
+		const questions = [
+			{
+				questionText: 'Quale città ha più abitanti?',
+				answerOptions: [
+					{ answerText: 'New York', isCorrect: false },
+					{ answerText: 'Pechino', isCorrect: false },
+					{ answerText: 'New Dheli', isCorrect: true },
+					{ answerText: 'Tokyo', isCorrect: false },
+				],
+			},
+			{
+				questionText: 'Quali sono i colori sociali di Roma?',
+				answerOptions: [
+					{ answerText: 'Bianco e Celeste', isCorrect: false },
+					{ answerText: 'Giallo e Rosso', isCorrect: true },
+					{ answerText: 'Giallo e Blu', isCorrect: false },
+					{ answerText: 'Bianco e Giallo', isCorrect: false },
+				],
+			},
+			{
+				questionText: 'Quale animale gloglotta?',
+				answerOptions: [
+					{ answerText: 'Tacchino', isCorrect: true },
+					{ answerText: 'Passerotto', isCorrect: false },
+					{ answerText: 'Cane', isCorrect: false },
+					{ answerText: 'Maiale', isCorrect: false },
+				],
+			},
+			{
+				questionText: 'In che anno è stata scoperta lAmerica',
+				answerOptions: [
+					{ answerText: '1512', isCorrect: false },
+					{ answerText: '1500', isCorrect: false },
+					{ answerText: '1489', isCorrect: false },
+					{ answerText: '1492', isCorrect: true },
+				],
+			},
+	    {
+				questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+				answerOptions: [
+					{ answerText: 'Italia', isCorrect: false },
+					{ answerText: 'Brasile', isCorrect: true },
+					{ answerText: 'Francia', isCorrect: false },
+					{ answerText: 'Inghilterra', isCorrect: false },
+				],
+			},
+	    {
+				questionText: 'La Bandiera di quale nazione non ha una forma rettangolare',
+				answerOptions: [
+					{ answerText: 'St.Kitts & Nevis', isCorrect: false },
+					{ answerText: 'Nepal', isCorrect: true },
+					{ answerText: 'Isole Vergini Britanniche ', isCorrect: false },
+					{ answerText: 'Taipei', isCorrect: false },
+				],
+			},
+	    {
+				questionText: 'Quando è finita la Seconda Guerra Mondiale',
+				answerOptions: [
+					{ answerText: '1943', isCorrect: false },
+					{ answerText: '1945', isCorrect: true },
+					{ answerText: '1944', isCorrect: false },
+					{ answerText: '1946', isCorrect: false },
+				],
+			},
+	    {
+				questionText: 'Quali città furono colpite dalla bomba atomica?',
+				answerOptions: [
+					{ answerText: ' Sapporo e Sendai', isCorrect: false },
+					{ answerText: ' Hiroshima e Nagasaki', isCorrect: true },
+					{ answerText: ' Kyoto e Nagoya', isCorrect: false },
+					{ answerText: 'Kumamoto e Sagamihara ', isCorrect: false },
+				],
+			},
+	    {
+				questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+				answerOptions: [
+					{ answerText: ' ', isCorrect: false },
+					{ answerText: ' ', isCorrect: true },
+					{ answerText: ' ', isCorrect: false },
+					{ answerText: ' ', isCorrect: false },
+				],
+			},
+	    {
+				questionText: 'Quale tra le seguenti nazionali ha vinto 5 mondiali?',
+				answerOptions: [
+					{ answerText: ' ', isCorrect: false },
+					{ answerText: ' ', isCorrect: true },
+					{ answerText: ' ', isCorrect: false },
+					{ answerText: ' ', isCorrect: false },
+				],
+			},
+		];*/
+	
 
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [showScore, setShowScore] = useState(false);
